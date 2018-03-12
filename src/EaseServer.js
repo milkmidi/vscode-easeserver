4d711fa67871a04ad6b3f2f47d4b5e9c318aceac
milkmidi <milkmidi@gmail.com>
2018-03-12 14:09:31 +0800

    add eslint

--- src/EaseServer.js
+++ src/EaseServer.js
@@ -1,111 +1,109 @@
-// 'use strict';
-const express = require( 'express' );
-const http = require( 'http' );
-const vscode = require( 'vscode' );
-const fs = require('fs');
+const vscode = require('vscode');
 const watch = require('node-watch');
-const browserSync = require("browser-sync");
-const window = vscode.window;
+const browserSync = require('browser-sync');
 
-const WATCH_FILE_NAME =  /\.(html|htm|css|js)$/;
+const { window } = vscode;
+
+const WATCH_FILE_NAME = /\.(html|htm|css|js)$/;
 const SERVER_STOP = 0;
 const SERVER_START = 1;
 const SERVER_RUNNING = 2;
 
-/** 
- * @Class EaseServer. 
+/**
+ * @Class EaseServer.
  * */
 class EaseServer {
+  /**
+ * @constructor
+ * @param  {number} portNumber=9527
+ * @param  {number} uiportNumber=9528
+ * @param  {string} wwwRoot=""
+ */
+  constructor(portNumber = 9527, uiportNumber = 9528, wwwRoot = '') {
     /**
-     * @constructor
-     * @param  {number} portNumber=9527
-     * @param  {number} uiportNumber=9528
-     * @param  {string} wwwRoot=""
+     * private
      */
-    constructor( portNumber = 9527 , uiportNumber = 9528 , wwwRoot = "" ) {
-        /**
-         * private
-         */        
-        this.server = null;
-        /**
-         * private
-         * @type {StatusBarItem}
-         */
-        this.statusBarItem = window.createStatusBarItem( vscode.StatusBarAlignment.Right, 1 );
-        /**
-         * @type {number}
-         */
-        this.state = SERVER_STOP;
-        /**
-         * @type {number}
-         */        
-        this.portNumber = portNumber;
-        this.uiportNumber = uiportNumber;
-        /**
-         * @type {string}
-         */
-        this.wwwRoot = wwwRoot;
-    }
-    
+    this.server = null;
     /**
-     * start ease server
-     */
-    start() {        
-        return new Promise(( resolve, reject ) => {
-            if ( this.state === SERVER_START ) {
-                resolve();
-                return;
-            }
-            this.state = SERVER_RUNNING;
-            this.server = browserSync.create();
-            this.server.init( {
-                files   :'**/*.html',
-                open    : false,
-                port    : this.portNumber,
-                server  : this.wwwRoot,
-                ui: {
-                    port: this.uiportNumber
-                }
-            } );
-            watch(this.wwwRoot, { recursive: true }, (evtType , filename)=>{
-                if (evtType === 'update' && WATCH_FILE_NAME.test(filename)) {
-                    this.server.reload();
-                }
-            })
-            this.state = SERVER_START;
-            this.statusBarItem.command = "easeserver.stop";
-            // this._statusBarItem.command = "easeserver.openInBrowser";
-            this.statusBarItem.tooltip = "Click here to stop easeServer";
-            this.statusBarItem.text = "Stop easeServer";
-            // this._statusBarItem.text = `http://localhost:${this.portNumber}`;
-            this.statusBarItem.show();
-            resolve();
-        });
-    }
+    * private
+    * @type {StatusBarItem}
+    */
+    this.statusBarItem = window.createStatusBarItem(vscode.StatusBarAlignment.Right, 1);
+    /**
+    * @type {number}
+    */
+    this.state = SERVER_STOP;
+    /**
+    * @type {number}
+    */
+    this.portNumber = portNumber;
+    this.uiportNumber = uiportNumber;
+    /**
+    * @type {string}
+    */
+    this.wwwRoot = wwwRoot;
+  }
 
-    /**
-     * stop server
-     */
-    stop() {
-        return new Promise(( resolve, reject ) => {
-            if ( this.server === null ) {
-                reject( "Server not running" );
-            } else {
-                this.server.exit();
-                this.server = null;
-                this.state = SERVER_STOP;
-                if( this.statusBarItem !== null){
-                    this.statusBarItem.hide();
-                }
-                resolve();
-            }
-        });
-    }
-    dispose() {
-        this.stop();        
-        this.statusBarItem.dispose();
-        this.statusBarItem = null;        
-    }
+  /**
+  * start ease server
+  */
+  start() {
+    return new Promise((resolve) => {
+      if (this.state === SERVER_START) {
+        resolve();
+        return;
+      }
+      this.state = SERVER_RUNNING;
+      this.server = browserSync.create();
+      this.server.init({
+        files: '**/*.html',
+        open: false,
+        port: this.portNumber,
+        server: this.wwwRoot,
+        ui: {
+          port: this.uiportNumber,
+        },
+      });
+      watch(this.wwwRoot, { recursive: true }, (evtType, filename) => {
+        if (evtType === 'update' && WATCH_FILE_NAME.test(filename)) {
+          this.server.reload();
+        }
+      });
+      this.state = SERVER_START;
+      this.statusBarItem.command = 'easeserver.stop';
+      // this._statusBarItem.command = "easeserver.openInBrowser";
+      this.statusBarItem.tooltip = 'Click here to stop easeServer';
+      this.statusBarItem.text = 'Stop easeServer';
+      // this._statusBarItem.text = `http://localhost:${this.portNumber}`;
+      this.statusBarItem.show();
+      window.showInformationMessage('EaseServer Start');
+      resolve();
+    });
+  }
+
+  /**
+  * stop server
+  */
+  stop() {
+    return new Promise((resolve, reject) => {
+      if (this.server === null) {
+        reject(new Error('Server not running'));
+      } else {
+        this.server.exit();
+        this.server = null;
+        this.state = SERVER_STOP;
+        if (this.statusBarItem !== null) {
+          this.statusBarItem.hide();
+        }
+        resolve();
+      }
+    });
+  }
+  dispose() {
+    this.stop();
+    this.statusBarItem.dispose();
+    this.statusBarItem = null;
+  }
 }
 
 module.exports = EaseServer;
--- src/extension.js
+++ src/extension.js
@@ -1,42 +1,55 @@
-/// <reference path="../node_modules/open/lib/open.js" />
+// / <reference path="../node_modules/open/lib/open.js" />
 // node_modules
-const vscode = require( 'vscode' );
-const open = require( 'open' );
+const vscode = require('vscode');
+const open = require('open');
 // my class
-const EaseServer = require( './EaseServer' );
+const EaseServer = require('./EaseServer');
 // instance
-const config = vscode.workspace.getConfiguration( 'easeServer' ) || {};
+const config = vscode.workspace.getConfiguration('easeServer') || {};
 const wwwRoot = vscode.workspace.rootPath;
+
+
+const openInBrowser = () => {
+  const edit = vscode.window.activeTextEditor;
+  if (!edit) {
+    return;
+  }
+  const fileName = edit.document.fileName.replace(wwwRoot, '');
+  console.log(`openInBrowser: fileName=${fileName}`);
+  open(`http://localhost:${config.portNumber}${fileName}`);
+};
+
+
 /**
  * @param  {vscode.ExtensionContext} context
  */
-function activate( context ) {
-    console.log( '"EaseServer" is now active!' );
+function activate(context) {
+  console.log('"EaseServer" is now active!');
 
-    var server = new EaseServer( config.portNumber , config.uiportNumber, wwwRoot );
-    context.subscriptions.push( server );
+  const server = new EaseServer(config.portNumber, config.uiportNumber, wwwRoot);
+  context.subscriptions.push(server);
 
-    var disposable;
-    disposable = vscode.commands.registerCommand( 'easeserver.start', () => {
-        server.start().then(() => {
-            openInBrowser();
-        }).catch( err => {
-            vscode.window.showInformationMessage( 'EaseServer fail:' + err );
-        });
+  let disposable;
+  disposable = vscode.commands.registerCommand('easeserver.start', () => {
+    server.start().then(() => {
+      openInBrowser();
+    }).catch((err) => {
+      vscode.window.showInformationMessage(`EaseServer fail:${err}`);
     });
-    context.subscriptions.push( disposable );
+  });
+  context.subscriptions.push(disposable);
 
-    disposable = vscode.commands.registerCommand( 'easeserver.stop', () => {
-        server.stop().then(() => {
-            vscode.window.showInformationMessage( 'EaseServer has been stopped' );
-        });
+  disposable = vscode.commands.registerCommand('easeserver.stop', () => {
+    server.stop().then(() => {
+      vscode.window.showInformationMessage('EaseServer has been stopped');
     });
-    context.subscriptions.push( disposable );
+  });
+  context.subscriptions.push(disposable);
 
-    disposable = vscode.commands.registerCommand( 'easeserver.openInBrowser', () => {
-        openInBrowser();
-    });
-    context.subscriptions.push( disposable );
+  disposable = vscode.commands.registerCommand('easeserver.openInBrowser', () => {
+    openInBrowser();
+  });
+  context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
@@ -45,12 +58,3 @@ function deactivate() {
 }
 exports.deactivate = deactivate;
 
-function openInBrowser() {
-    var edit = vscode.window.activeTextEditor;
-    if ( !edit ) {
-        return;
-    }
-    var fileName = edit.document.fileName.replace( wwwRoot, "" );
-    console.log( "openInBrowser: fileName=" + fileName );
-    open( `http://localhost:${config.portNumber}` + fileName );
-}
